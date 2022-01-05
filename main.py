ef1275f6740efe20233265cc5217ed9a55d4bc20
johnchowssc <71998398+johnchowssc@users.noreply.github.com>
2022-01-05 16:07:48 +1100

    comment out favicon route

--- main.py
+++ main.py
@@ -156,10 +156,10 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ## Routes
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def get_all_posts():
